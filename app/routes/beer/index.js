1ada175362cb188d9fc98db75766ea5498f6ee77
jonpitch <jon.pitcherella@gmail.com>
2015-03-19 08:51:18 -0400

    refactoring list of places into component.

--- app/routes/beer/index.js
+++ app/routes/beer/index.js
@@ -12,14 +12,7 @@ export default Ember.Route.extend({
             ibu: 70,
             srm: 9,
             brewery: ['Victory Brewing Company'],
-            img: 'https://s3.amazonaws.com/brewerydbapi/beer/YTNQI4/upload_Gxu5u4-large.png',
-            places: [
-                { id: 1, name: 'Place A', verified: true, updated: 1426701569, lat: 1234, lng: 1234, img: 'http://placehold.it/48x48', menu: [1, 2, 3] },
-                { id: 2, name: 'Place B', verified: true, updated: 1426701000, lat: 1234, lng: 1234, img: 'http://placehold.it/48x48', menu: [1] },
-                { id: 3, name: 'Place C', verified: true, updated: 1426700569, lat: 1234, lng: 1234, img: 'http://placehold.it/48x48', menu: [1, 2, 3, 4, 5] },
-                { id: 4, name: 'Place D', verified: false, updated: 1426700100, lat: 1234, lng: 1234, img: 'http://placehold.it/48x48', menu: [1, 2, 3, 7, 8, 9, 10] },
-                { id: 5, name: 'Place E', verified: true, updated: 1426691569, lat: 1234, lng: 1234, img: 'http://placehold.it/48x48', menu: [1, 2] },
-            ]
+            img: 'https://s3.amazonaws.com/brewerydbapi/beer/YTNQI4/upload_Gxu5u4-large.png'
         };
     }
 });
--- app/routes/places/index.js
+++ app/routes/places/index.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function () {
-        return [
-            { id: 1, name: 'Place 1', verified: true, updated: 1426701569, lat: 39.94376230, lng: -75.16759820, img: 'http://placehold.it/48x48', menu: [1, 2, 3] },
-            { id: 2, name: 'Place 2', verified: true, updated: 1426701000, lat: 39.96207930, lng: -75.14112650, img: 'http://placehold.it/48x48', menu: [4] },
-            { id: 3, name: 'Place 3', verified: false, updated: 1426700569, lat: 39.95016720, lng: -75.16260730, img: 'http://placehold.it/48x48', menu: [1, 2, 3, 4, 5, 6] },
-            { id: 4, name: 'Place 4', verified: true, updated: 1426700100, lat: 39.95022100, lng: -75.16658300, img: 'http://placehold.it/48x48', menu: [2, 3] },
-            { id: 5, name: 'Place 5', verified: true, updated: 1426691569, lat: 39.94131370, lng: -75.15246310, img: 'http://placehold.it/48x48', menu: [] }
-        ];
-    }
 });
